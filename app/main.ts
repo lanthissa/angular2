3732e6ba283492548d1bf133bf7c0c0c8bbc4544
Andrew Bilgore <andrew.bilgore@gmail.com>
2016-01-31 10:28:16 -0500

    tutorial section 2 completed

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,6 @@
 /**
- * Created by andrewbilgore on 1/28/16.
+ * Created by andrewbilgore on 1/30/16.
  */
-
 import {bootstrap} from 'angular2/platform/browser';
 import  {AppComponent} from './app.component'
 
